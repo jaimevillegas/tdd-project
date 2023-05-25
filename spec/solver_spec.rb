c79f43ab3d8f653391a93b4e815f5be05c4b07d8
David Espino <davidespinog@gmail.com>
2023-05-25 10:20:24 -0600

    Add tests for reverse and fizzBuzz

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -24,4 +24,28 @@ describe Solver do
       expect(operation1.factorial(6)).to eql(720)
     end
   end
+
+  describe "#reverse" do 
+    it "Should return 'olleh' when the string is 'hello'" do
+      expect(operation1.reverse("hello")).to eql("olleh")
+    end
+    it "Should return 'dlrow' when the string is 'world'" do
+      expect(operation1.reverse("world")).to eql("dlrow")
+    end
+  end
+
+  describe "#fizzbuzz" do
+    it "Should return 'Fizz' when the number is divisible by 3" do
+      expect(operation1.fizzbuzz(3)).to eql("Fizz")
+    end
+    it "Should return 'Buzz' when the number is divisible by 5" do
+      expect(operation1.fizzbuzz(5)).to eql("Buzz")
+    end
+    it "Should return 'FizzBuzz' when the number is divisible by 3 and 5" do
+      expect(operation1.fizzbuzz(15)).to eql("FizzBuzz")
+    end 
+    it "Should return a stringified number when the number is not divisible by 3 or 5" do
+      
+    end
+  end
 end
